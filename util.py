f370f5b7bc8a40e64765997d312e177c4e0e104b
mikita <mikita.maliauka@gmail.com>
2025-03-03 13:37:20 +0300

    looking for stable result...

--- util.py
+++ util.py
@@ -1,8 +1,20 @@
 import numpy as np
 
-def read_pd_from_file(filename: str) -> np.array:
+def read_pd_from_file_limited(filename: str) -> np.array:
     with open(filename, 'r') as file:
         lines = file.readlines()[:100]
+        result = np.zeros(len(lines), dtype=np.float32)
+
+        for i, l in enumerate(lines):
+            result[i] = float(l)
+        
+        return result 
+
+
+
+def read_pd_from_file(filename: str) -> np.array:
+    with open(filename, 'r') as file:
+        lines = file.readlines()
         result = np.zeros(len(lines), dtype=np.float32)
 
         for i, l in enumerate(lines):
